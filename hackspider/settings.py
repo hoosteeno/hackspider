ce03197258a5b4a601d9dacc045f181d061fe238
hoosteeno <justin@tentacu.com>
2014-12-11 16:02:40 -0700

    add crawling

--- hackspider/settings.py
+++ hackspider/settings.py
@@ -12,6 +12,8 @@ BOT_NAME = 'hackspider'
 
 SPIDER_MODULES = ['hackspider.spiders']
 NEWSPIDER_MODULE = 'hackspider.spiders'
+DOWNLOAD_DELAY = 1.25
+COOKIES_ENABLED = False
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'hackspider (+http://www.yourdomain.com)'
--- hackspider/spiders/hackspider.py
+++ hackspider/spiders/hackspider.py
@@ -1,14 +1,31 @@
-from scrapy import Spider, Item, Field
+import re
+from scrapy import Item, Field, Spider
+from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
 
 class Page(Item):
-    img = Field()
+    url = Field()
+    imgs = Field()
 
-class HackSpider(Spider):
-    name = 'hackspider'
-    start_urls = ['http://hacks.mozilla.org']
-    allowed_domains = ['hacks.mozilla.org']
+class HackSpider(CrawlSpider):
+#class HackSpider(Spider):
+    name = "hackspider"
+    allowed_domains = ["hacks.mozilla.org"]
+    start_urls = ["http://hacks.mozilla.org"]
 
-    def parse(self, response):
-        #print(response.css("img").xpath('@src').extract())
-        return [Page(img = e.extract()) for e in response.css("img").xpath('@src')]
+    rules = (
+        Rule(LinkExtractor(), follow=True, callback='parse_item'),
+    )
+
+    def parse_item(self, response):
+    #def parse(self, response):
+        imgs = []
+
+        for e in response.css("img").xpath("@src"):
+            img = e.extract()
+            if (re.search('-\d+x\d+\.(png|jpg|gif)', img)):
+                imgs.append(img)
+
+        if imgs:
+            return Page(url=response.url, imgs=imgs)
 
